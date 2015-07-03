47802a0cbfcf58b38244ea6f5074693df25574e3
Basov1981 <Basov1981@users.noreply.github.com>
2015-07-03 18:56:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,214 @@
-/**
- * Created by IBAH on 30.06.15.
- */
-public class Main {
+import java.util.*;
+
+
+public class Order implements  Comparable<Order>{
+    private Set<Order> musor= new TreeSet<>();
+    int numberOrd;
+    int statusOrd;
+    Date dateCreate;
+    private Map < Integer, Produkt> korzina = new TreeMap<>();
+
+    private static int maxNumber ;
+
+    public Order() {
+       this.numberOrd=maxNumber+1;
+       this.dateCreate=new Date();
+        musor.add(this);
+
+    }
+
     public static void main(String[] args) {
-        System.out.println("hdhhdfghd");
-        System.out.println("kgkjfdgkjf");
+       new Order();
+    }
+
+    public void addIten(Produkt pr, int countProd ){
+       korzina.
+    }
+
+    @Override
+    public int compareTo(Order or) {
+
+        return  this.numberOrd - or.numberOrd;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + "Order{" +
+                "dateCreate=" + dateCreate +
+                ", numberOrd=" + numberOrd +
+                '}';
     }
 }
+
+
+import java.util.*;
+
+
+public class Order implements  Comparable<Order>{
+    private Set<Order> musor= new TreeSet<>();
+    int numberOrd;
+    int statusOrd;
+    Date dateCreate;
+    private Map < Integer, Produkt> korzina = new TreeMap<>();
+
+    private static int maxNumber ;
+
+    public Order() {
+       this.numberOrd=maxNumber+1;
+       this.dateCreate=new Date();
+        musor.add(this);
+
+    }
+
+    public static void main(String[] args) {
+       new Order();
+    }
+
+    public void addIten(Produkt pr, int countProd ){
+       korzina.
+    }
+
+    @Override
+    public int compareTo(Order or) {
+
+        return  this.numberOrd - or.numberOrd;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + "Order{" +
+                "dateCreate=" + dateCreate +
+                ", numberOrd=" + numberOrd +
+                '}';
+    }
+}
+
+
+import java.util.*;
+
+
+public class Order implements  Comparable<Order>{
+    private Set<Order> musor= new TreeSet<>();
+    int numberOrd;
+    int statusOrd;
+    Date dateCreate;
+    private Map < Integer, Produkt> korzina = new TreeMap<>();
+
+    private static int maxNumber ;
+
+    public Order() {
+       this.numberOrd=maxNumber+1;
+       this.dateCreate=new Date();
+        musor.add(this);
+
+    }
+
+    public static void main(String[] args) {
+       new Order();
+    }
+
+    public void addIten(Produkt pr, int countProd ){
+       korzina.
+    }
+
+    @Override
+    public int compareTo(Order or) {
+
+        return  this.numberOrd - or.numberOrd;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + "Order{" +
+                "dateCreate=" + dateCreate +
+                ", numberOrd=" + numberOrd +
+                '}';
+    }
+}
+
+
+import java.util.*;
+
+
+public class Order implements  Comparable<Order>{
+    private Set<Order> musor= new TreeSet<>();
+    int numberOrd;
+    int statusOrd;
+    Date dateCreate;
+    private Map < Integer, Produkt> korzina = new TreeMap<>();
+
+    private static int maxNumber ;
+
+    public Order() {
+       this.numberOrd=maxNumber+1;
+       this.dateCreate=new Date();
+        musor.add(this);
+
+    }
+
+    public static void main(String[] args) {
+       new Order();
+    }
+
+    public void addIten(Produkt pr, int countProd ){
+       korzina.
+    }
+
+    @Override
+    public int compareTo(Order or) {
+
+        return  this.numberOrd - or.numberOrd;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + "Order{" +
+                "dateCreate=" + dateCreate +
+                ", numberOrd=" + numberOrd +
+                '}';
+    }
+}
+
+
+import java.util.*;
+
+
+public class Order implements  Comparable<Order>{
+    private Set<Order> musor= new TreeSet<>();
+    int numberOrd;
+    int statusOrd;
+    Date dateCreate;
+    private Map < Integer, Produkt> korzina = new TreeMap<>();
+
+    private static int maxNumber ;
+
+    public Order() {
+       this.numberOrd=maxNumber+1;
+       this.dateCreate=new Date();
+        musor.add(this);
+
+    }
+
+    public static void main(String[] args) {
+       new Order();
+    }
+
+    public void addIten(Produkt pr, int countProd ){
+       korzina.
+    }
+
+    @Override
+    public int compareTo(Order or) {
+
+        return  this.numberOrd - or.numberOrd;
+    }
+
+    @Override
+    public String toString() {
+        return "\n" + "Order{" +
+                "dateCreate=" + dateCreate +
+                ", numberOrd=" + numberOrd +
+                '}';
+    }
+}
+
